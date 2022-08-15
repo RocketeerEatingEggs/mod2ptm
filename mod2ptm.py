1a026fbacea0fbcab83ffb7a15d24942abfa5976
RocketeerEatingEggs <94308822+RocketeerEatingEggs@users.noreply.github.com>
2022-08-15 19:14:17 -0400

    Update mod2ptm.py

--- mod2ptm.py
+++ mod2ptm.py
@@ -1,6 +1,4 @@
 # mod2ptm.py
-# a (probably) useless tool that converts from ProTracker .mod to Poly Tracker .ptm
-# the reason I say useless is because it's obvious it's a converter based on the files it spits out
 # by RocketeerEatingEggs
 
 import sys
@@ -23,8 +21,7 @@ ModPeriodTable = [
     ]
 
 ModFinetunesTable = [
-    8272, 8332, 8393, 8453, 8514, 8576, 8638, 8701, # 7808, 7864, 7921, 7978, 8037, 8095, 8153, 8213
-    8272, 8332, 8393, 8453, 8514, 8576, 8638, 8701
+    8272, 8332, 8393, 8453, 8514, 8576, 8638, 8701, 7808, 7864, 7921, 7978, 8037, 8095, 8153, 8213
     ]
 
 ModChannelsTable = [
@@ -144,7 +141,10 @@ with open(sys.argv[2], "wb") as PTMfile:
                 PTMfile.write(b"\x00\x00\x00\x00")
                 PTMfile.write(smpLengthBytes)
                 PTMfile.write(smpRepStartBytes)
-                PTMfile.write(smpRepEndBytes)
+                if smpRepLength <= 2:
+                    PTMfile.write(b"\x00\x00\x00\x00")
+                else:
+                    PTMfile.write(smpRepEndBytes)
                 PTMfile.write(b"\x00\x00\x00\x00")
                 PTMfile.write(b"\x00\x00\x00\x00")
                 PTMfile.write(b"\x00\x00\x00\x00")
@@ -172,6 +172,7 @@ with open(sys.argv[2], "wb") as PTMfile:
                         note = periodToNote(lowerPeriod, upperPeriod)
                         instrument = (((eventPart1 & 240) << 4) | (eventPart3 & 240)) >> 4
                         effectNumber = eventPart3 & 15
+                        volumeParam = effectParam
                         noNote = True
                         if note != 0:
                             noNote = False
